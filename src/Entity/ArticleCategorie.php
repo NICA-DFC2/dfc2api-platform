b1632e6acc9084c3894decc69959bee780cdc7dc
NICA-DFC2 <n.cartier@dfc2.biz>
2018-11-08 15:15:19 +0100

    modifications du service

--- src/Entity/ArticleCategorie.php
+++ src/Entity/ArticleCategorie.php
@@ -32,7 +32,7 @@ class ArticleCategorie
 
     /**
      * @Gedmo\Slug(fields={"name"})
-     * @ORM\Column(name="slug", type="string", length=255, unique=true)
+     * @ORM\Column(name="slug", type="string", length=510, unique=true)
      */
     private $slug;
 
