422b5779a144a968648357c20732f149a77cb45d
NICA-DFC2 <n.cartier@dfc2.biz>
2018-11-08 14:40:13 +0100

    modifications du service

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -19,6 +19,22 @@ use JMS\Serializer\Annotation as JMSSerializer;
  *     "normalization_context"={"groups"={"user", "user-read"}},
  *     "denormalization_context"={"groups"={"user", "user-write"}}
  * })
+ * @ORM\AttributeOverrides({
+ *      @ORM\AttributeOverride(name="usernameCanonical",
+ *          column=@ORM\Column(
+ *              name     = "username_canonical",
+ *              length   = 191,
+ *              unique   = true
+ *          )
+ *      ),
+ *      @ORM\AttributeOverride(name="emailCanonical",
+ *          column=@ORM\Column(
+ *              name     = "email_canonical",
+ *              length   = 191,
+ *              unique   = true
+ *          )
+ *      )
+ * })
  */
 class User extends BaseUser
 {
