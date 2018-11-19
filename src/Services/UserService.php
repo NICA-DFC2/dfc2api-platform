73a0a873e0c77bd083c7f6ccf65bc09143bfd071
NICA-DFC2 <n.cartier@dfc2.biz>
2018-11-19 11:19:40 +0100

    modifications security

--- src/Services/UserService.php
+++ src/Services/UserService.php
@@ -59,7 +59,7 @@ class UserService
             'lien_bl' => null,
             'lien_dev' => null,
             'lien_fac' => null,
-            'lien_panier' => null
+            'lien_paniers' => null
         ];
     }
 
@@ -136,7 +136,7 @@ class UserService
                             $this->user_infos['lien_cmd'] = 'api/ws/commandes/'.$wsClient->getIdCli().'/client';
                             $this->user_infos['lien_dev'] = 'api/ws/devis/'.$wsClient->getIdCli().'/client';
                             $this->user_infos['lien_fac'] = 'api/ws/factures/'.$wsClient->getIdCli().'/client';
-                            $this->user_infos['lien_panier'] = 'api/paniers?user.id_cli='.$wsClient->getIdCli();
+                            $this->user_infos['lien_paniers'] = 'api/paniers?user.id_cli='.$wsClient->getIdCli();
                             //$this->user_infos['interface'] = serialize($token);
 
                         }
